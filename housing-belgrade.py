5068f166cf805d66a0976da8672c38c717078ce0
MilijanS <stojnic.milijan@gmail.com>
2017-02-11 15:53:47 +0100

    Added multithreaded functionality, huge performance increase

--- housing-belgrade.py
+++ housing-belgrade.py
@@ -3,12 +3,14 @@ import random
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
-
+import concurrent.futures
 
 
 atributi = ['Grad', 'Opstina', 'Naselje', 'Ulica', 'Tip', 'Kvadratura', 'Broj soba', 'Cena']
 
 baza = pd.DataFrame(columns=atributi)
+
+halo_oglasi = r'https://www.halooglasi.com/nekretnine/prodaja-stanova/beograd?cena_d_from=5000&cena_d_unit=4&page='
 
 class Nekretnina:
     
@@ -43,9 +45,10 @@ def dodaj_nekretninu_u_dataframe(df):
     pass
 
 #1125
-for i in range(0, 1100):
-            
-    url = r'https://www.halooglasi.com/nekretnine/prodaja-stanova/beograd?cena_d_from=5000&cena_d_unit=4&page=' + str(i)
+
+def process_single_page(i):
+    
+    url = halo_oglasi + str(i)
 
     page = urlopen(url)
 
@@ -101,8 +104,25 @@ for i in range(0, 1100):
         except:
             pass 
                 
-print(baza)
-baza.to_csv('cene stanova.csv', encoding='utf-8')
+
+def process_website_pages(number_of_pages):
+    
+    try:
+        
+        with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:            
+            future_to_i = {executor.submit(process_single_page, i): i for i in range(0, number_of_pages)}         
+            
+    finally:
+        
+        print("End.\n")
+        print(baza)
+    
+
+
+
+
+
+#baza.to_csv('cene stanova.csv', encoding='utf-8')
 
         
 
