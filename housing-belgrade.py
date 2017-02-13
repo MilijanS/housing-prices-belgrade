4a05d22f677bea41fb96a0efd56396ec3490a901
MilijanS <stojnic.milijan@gmail.com>
2017-02-13 20:41:31 +0100

    prikupljanje jsona sa detaljnim informacijama o nekretnini

--- housing-belgrade.py
+++ housing-belgrade.py
@@ -5,15 +5,20 @@ import html5lib
 import re
 import pandas as pd
 import concurrent.futures
+import json
+import pickle
 
 atributi = ['Grad', 'Opstina', 'Naselje', 'Ulica', 'Tip', 'Kvadratura', 'Broj soba', 'Cena']
 
 parser = 'lxml'
 
+stringic = ''
 
 dataframe = pd.DataFrame(columns=atributi)
 baza = []
+baza_jsona = []
 
+halo_oglasi = r'https://www.halooglasi.com'
 halo_oglasi_stanovi_beograd = r'https://www.halooglasi.com/nekretnine/prodaja-stanova/beograd?cena_d_from=5000&cena_d_unit=4'
 halo_oglasi_kuce_beograd = r'https://www.halooglasi.com/nekretnine/prodaja-kuca/beograd?cena_d_from=4000&cena_d_unit=4'
 
@@ -121,7 +126,52 @@ def process_single_page(web_page, page_index):
         
     print('Ubacio nekretnine sa stranice {0}'.format(page_index))    
                 
+    
+def get_jsons(web_page, page_index):
+    
+    print("Obradjujem stranicu {0}".format(page_index))
+    
+    url = web_page + r'&page=' + str(page_index)
 
+    page = urlopen(url)
+
+    soup = BeautifulSoup(page, parser)
+    
+    # mydivs sadrzi 20 razlicitih stanova
+    mydivs = soup.findAll('div', { 'class' : 'col-md-12 col-sm-12 col-xs-12 col-lg-12'})
+    
+    # obradi 20 nekretnina
+    for i, div in enumerate(mydivs):
+        try:
+            global stringic
+            stranica_nekretnine = div.find('a')
+            stranica_nekretnine = stranica_nekretnine.get('href')
+            
+            url_nekretnine_string = halo_oglasi + stranica_nekretnine
+            url_nekretnine = urlopen(url_nekretnine_string)
+            soup_nekretnine = BeautifulSoup(url_nekretnine, parser)
+            
+            scripts = soup_nekretnine.find_all('script')
+            raw_json = scripts[16].text
+            json_string = re.findall(r'(QuidditaEnvironment.CurrentClassified=)(.*)(;.)(for \(var i in QuidditaEnvironment)', raw_json)[0]
+            jjson = json_string[1]
+
+            stringic = jjson
+            
+            baza_jsona.append(jjson)  
+            
+            jsonucitan = json.loads(stringic)
+            print(jsonucitan['OtherFields']['ulica_t'])
+                            
+        except Exception as e:
+            print(e) 
+        finally:
+            pass
+        
+    print('Ubacio nekretnine sa stranice {0}'.format(page_index))    
+                
+    
+    
 def process_website_pages(web_page, start_page, end_page):
     
     try:
@@ -146,3 +196,13 @@ def prikupi_cene_stanova_i_kuca_u_beogradu():
 def eksportuj_kao_csv(naziv='Cene kuca i stanova'):
     
     dataframe.to_csv(naziv, encoding='utf-8')
+    
+def pickle_json_array():
+        
+    pickle.dump(baza_jsona, open('baza jsona.pickle', 'wb'))
+
+def unpickle_json_array(filename):
+    return pickle.load(open(filename, 'rb'))
+    
+
+get_jsons(halo_oglasi_stanovi_beograd, 1)
